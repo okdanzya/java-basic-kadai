3fec3e8a0df117a47e6f88f34cf06acbb22978b3
okdanzya <kei1ota.ao@gmail.com>
2025-10-05 08:24:25 +0900

    Javaファイルの更新

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -5,23 +5,34 @@ public class Car_Chapter15 {
     private int speed = 10;
 
     public void changeGear(int afterGear) {
+    	int before = this.gear; // 切り替え前を保持
         this.gear = afterGear;
-        if (gear == 1) {
-            speed = 10;
-        } else if (gear == 2) {
-            speed = 20;
-        } else if (gear == 3) {
-            speed = 30;
-        } else if (gear == 4) {
-            speed = 40;
-        } else if (gear == 5) {
-            speed = 50;
-        } else {
-            speed = 10;
+        switch (gear) {
+        case 1:
+          speed = 10;
+          break;
+        case 2:
+          speed = 20;
+          break;
+        case 3:
+          speed = 30;
+          break;
+        case 4:
+          speed = 40;
+          break;
+        case 5:
+          speed = 50;
+          break;
+        default:
+          speed = 10;
+          break;
         }
+        
+        System.out.println("ギア" + before + "から" + gear + "に切り替えました");
+        
     }
 
     public void run() {
-        System.out.println("速度は時速 " + speed + " kmです。");
+        System.out.println("速度は時速" + speed + "kmです");
     }
 }
