8f70bd8e835e0b4c47a15657fe22628c31c30b42
okdanzya <kei1ota.ao@gmail.com>
2025-10-07 19:54:15 +0900

    kadai_018をアップロード

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,10 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+    public static void main(String[] args) {
+        KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
+        katoTaro.setGivenName();
+        katoTaro.execIntroduce();
+
+    }
+}
