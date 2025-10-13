3481a49ce77f7ca8b48bbf3fb2154319094eeb80
okdanzya <kei1ota.ao@gmail.com>
2025-10-13 14:56:11 +0900

    KatoExec_Chapter18.javaを修正

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -5,6 +5,16 @@ public class KatoExec_Chapter18 {
         KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
         katoTaro.setGivenName();
         katoTaro.execIntroduce();
+        System.out.println(""); // ここで1行スペース
+
+        KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
+        katoIchiro.setGivenName();
+        katoIchiro.execIntroduce();
+        System.out.println(""); // ここで1行スペース
+
+        KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();
+        katoHanako.setGivenName();
+        katoHanako.execIntroduce();
 
     }
 }
